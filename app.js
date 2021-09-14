944ad4f5d7b37c4dfeab39ed7b8025d40c6214ed
Lucky <vagyasri2011@gmail.com>
2021-09-14 12:41:16 +0200

    Add delete book event

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const list = document.querySelector('#display-books ul');
 
+// delete books
+list.addEventListener('click', (e) => {
+  if (e.target.className === 'remove') {
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
+});
